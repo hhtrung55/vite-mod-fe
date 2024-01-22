1625c7eb35ab0b61bfceb6fd13887427cef02a40
trung.hoang <trung.hoang@sotatek.com>
2024-01-22 15:26:27 +0700

    env base url remote

--- host/vite.config.js
+++ host/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     federation({
       name: "app",
       remotes: {
-        remoteApp: "http://localhost:5001/assets/remoteEntry.js",
+        remoteApp: `${process.env.VITE_BASE_REMOTE_APP_URL}/assets/remoteEntry.js`,
       },
       shared: ["react", "react-dom"],
     }),
